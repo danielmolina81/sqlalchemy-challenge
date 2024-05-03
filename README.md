22cdc829af42e2820e838a4d1c4bb2da2257ae0f
Daniel <danielmolina81@hotmail.com>
2024-05-03 01:19:48 -0400

    api final 3may119

--- README.md
+++ README.md
@@ -58,17 +58,28 @@ Now that you’ve completed your initial analysis, you’ll design a Flask API b
 
 
 - /api/v1.0/precipitation
+Last 12 months of precipitation data presented as a JSON dictionary with "Date" as Key and the "Precipitation" results as a list of values.
 
 ![](SurfsUp/Pics/12.png)
 
 - /api/v1.0/stations
+List of stations presented as a JSON list.
 
 ![](SurfsUp/Pics/13.png)
 
 - /api/v1.0/tobs
+Last 12 months of temperature observations for the most active station presented as a JSON list of "Date" and "Temperature" lists.
 
 ![](SurfsUp/Pics/14.png)
 
-- /api/v1.0/<start> and /api/v1.0/<start>/<end>
+- /api/v1.0/<start>
+Minimum, average and maximum temperatures calculated from the given start date to the end of the dataset presented as a JSON list.
 
 ![](SurfsUp/Pics/15.png)
+![](SurfsUp/Pics/16.png)
+
+/api/v1.0/<start>/<end>
+Minimum, average and maximum temperatures calculated from the given start and end dates presented as a JSON list
+
+![](SurfsUp/Pics/17.png)
+![](SurfsUp/Pics/18.png)
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -44,9 +44,17 @@ def landing():
     return (
         f"<h1>Hello! Welcome to my API.</h1>"
         f"<h2>The available routes are:</h2>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
+        f"<h3>/api/v1.0/precipitation<br/>"
+        f"To see the last 12 months of precipitation data.</h3>"
+        f"<h3>/api/v1.0/stations<br/>"
+        f"To see a list of all stations.</h3>"        
+        f"<h3>/api/v1.0/tobs<br/>"
+        f"To see the last 12 months of temperature observations for the most active station.</h3>"
+        f"<h3>/api/v1.0/YYYY-MM-DD<br/>"
+        f"To see the minimum, average and maximum temperatures calculated from the given start date to the end of the dataset.</h3>"
+        f"<h3>/api/v1.0/YYYY-MM-DD/YYYY-MM-DD<br/>"
+        f"To see the minimum, average and maximum temperatures calculated from the given start and end date.</h3>"        
+        f"Note: Data available between 2010-01-01 and 2017-08-23"
     )
 
 #################################################
@@ -70,17 +78,17 @@ def precipitation():
     # Create a query to retrieve the last 12 months of precipitation data
     precip = session.query(M.date, M.prcp).filter(M.date.between(one_year, most_recent)).all()
 
-    # Close Session
+    # Close session
     session.close() 
 
-    # Convert list to a dictionary
+    # Convert list to a dictionary with the date as Key and prcp as a list of values
     precip_dict = {}
     for date, prcp in precip:
         if date in precip_dict:
             precip_dict[date].append(prcp)
         else:
             precip_dict[date] = [prcp]
-
+        
     # Return JSON representation of dictionary
     return jsonify(precip_dict)
 
@@ -93,10 +101,10 @@ def stations():
     session = Session(engine)
 
     # Create a query to retrieve the list of stations
-    stations = session.query(M.station).distinct().all()
+    stations = session.query(S.station).all()
 
-    # Close Session
-    session.close() 
+    # Close session
+    session.close()
 
     # Convert into a list
     stations_list = [x[0] for x in stations]
@@ -125,24 +133,102 @@ def tobs():
     tobs = session.query(M.date, M.tobs).filter(M.station == active_station[0], \
                                 M.date.between(one_year, most_recent)).all()
 
-    # Close Session
+    # Close session
     session.close()
 
     # Convert into a list of lists
-    tobs_list = [x[1] for x in tobs]
+    tobs_list = [[x[0],x[1]] for x in tobs]
 
+    # Return JSON representation of list
     return jsonify(tobs_list)
 
-
-# /api/v1.0/<start>
-# /api/v1.0/<start>/<end>
-
 #################################################
-# Flask Routes
-#################################################
-### API Static Routes
-
 ### API Dynamic Route
+#################################################
+
+# <start> route
+@app.route("/api/v1.0/<start>")
+def start(start):
+    """Minimum, average and maximum temperatures calculated from a given start date until the end of the dataset."""
+
+    # Create session from Python to the DB
+    session = Session(engine)
+
+    # Find the most recent date in the data set
+    last_date = session.query(func.max(M.date)).scalar()
+
+    # Find the first date in the data set
+    first_date = session.query(func.min(M.date)).scalar()
+
+    # Check if start date exists in data base
+    if start >=first_date and start <= last_date:
+
+        # Query minimum, average and maximum temperature for a specified start date
+        temperatures = session.query(func.min(M.tobs), func.max(M.tobs), func.avg(M.tobs)) \
+                        .filter(M.date >= start).all()
+
+        # Close session
+        session.close()
+
+        # Convert into a list
+        temperatures_list = [[x[0],x[1],x[2]] for x in temperatures]
+
+        # Return JSON representation of list
+        return jsonify(temperatures_list)
+    
+    # Error message in case start date is not present in the dataset
+    return jsonify({"error": f"{start} is not present in the dataset."}), 404
+
+# /api/v1.0/<start>/<end> route
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start,end):
+    """Minimum, average and maximum temperatures calculated from the given start and end date."""
+
+    # Create session from Python to the DB
+    session = Session(engine)
+
+    # Find the most recent date in the data set
+    last_date = session.query(func.max(M.date)).scalar()
+
+    # Find the first date in the data set
+    first_date = session.query(func.min(M.date)).scalar()
+
+    # Check if start date is after end date
+    if end<start:
+
+        # Error message in case start date is after end date
+        return jsonify({"error": f"start date cannot be after end date."}), 404        
+    
+    # Check if start date does not exist in data base
+    elif start < first_date or start > last_date:
+        if end >= first_date and end <= last_date:
+
+            # Error message in case start date is not present in the dataset
+            return jsonify({"error": f"{start} is not present in the dataset."}), 404    
+    
+        else:
+        # Error message in case start and end dates are not present in the dataset
+            return jsonify({"error": f"{start} and {end} are not present in the dataset."}), 404        
+
+    # Check if end date does not exist in data base    
+    elif end < first_date or end > last_date:
+
+        # Error message in case end date is not present in the dataset
+        return jsonify({"error": f"{end} is not present in the dataset."}), 404    
+
+    else:
+        # Query minimum, average and maximum temperature for a specified start date
+        temperatures = session.query(func.min(M.tobs), func.max(M.tobs), func.avg(M.tobs)) \
+                        .filter(M.date >= start, M.date <= end).all()
+
+        # Close session
+        session.close()
+
+        # Convert into a list
+        temperatures_list = [[x[0],x[1],x[2]] for x in temperatures]
+
+        # Return JSON representation of list
+        return jsonify(temperatures_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
